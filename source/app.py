7cdc9f4589dfc8cc3080879c337bd05e4fa92a1b
lauramicaela-m <lmicaelamendez@gmail.com>
2021-07-16 09:52:45 -0300

    creamos ruta para cargar empleados

--- source/app.py
+++ source/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect 
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -17,14 +19,39 @@ def index():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    sql = "insert into empleados (nombre, correo, foto) values ('Laura', 'correodelaura@gmail.com', 'foto.jpg')";
+    sql = "SELECT * FROM empleados;"
 
     cursor.execute(sql)
 
+    empleados = cursor.fetchall()
+ 
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados=empleados)
+
+
+@app.route('/create')
+def create():
     
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods=["POST"])
+def store():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+
+    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s );"
+    datos = (_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+
+    conn.commit()
+    
+    return redirect ('/')
 
 
 if __name__== '__main__':
