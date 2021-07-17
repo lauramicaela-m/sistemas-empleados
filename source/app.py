b8f4864099ffa23fb48706bc3148c2f49ae281f1
lauramicaela-m <lmicaelamendez@gmail.com>
2021-07-17 11:43:41 -0300

    cree ruta delete

--- source/app.py
+++ source/app.py
@@ -3,7 +3,9 @@ from flask import render_template
 from flask import request
 from flask import redirect 
 from flaskext.mysql import MySQL
-from datetime import datetime 
+from datetime import datetime
+from pymysql import cursors
+import os 
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -13,6 +15,9 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'empleados'
 
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS  #guardamos la ruta como un valor en la app
+
 mysql.init_app(app)
 
 @app.route('/')
@@ -21,7 +26,6 @@ def index():
     cursor = conn.cursor()
 
     sql = "SELECT * FROM empleados;"
-
     cursor.execute(sql)
 
     empleados = cursor.fetchall()
@@ -50,11 +54,11 @@ def store():
 
     if _foto.filename != '':
         nuevoNombreFoto = tiempo + '_' + _foto.filename
-        _foto.save("uploads/" + nuevoNombreFoto)
+        _foto.save("source/uploads/"+ nuevoNombreFoto)
 
 
     sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s );"
-    datos = (_nombre, _correo, _foto.filename)
+    datos = (_nombre, _correo, nuevoNombreFoto)
 
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -65,8 +69,65 @@ def store():
     return redirect ('/')
 
 
-if __name__== '__main__':
-    app.run(debug=True)
-
-
 @app.route('/delete/<int:id>')
+def delete(id):
+    sql = f'DELETE FROM empleados WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = f'DELETE FROM empleados WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    empleado = cursor.fetchone()
+    conn.commit()
+
+    return render_template('empleados/edit.html', empleado=empleado)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.form['txtFoto']
+    id = request.form['txtId']
+
+    datos = (_nombre, _correo, id)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if _foto.filename != '':
+        now= datetime.now()
+        tiempo = now.strftime("%Y%H%M%S")
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+
+        sql = f'SELECT foto FROM empleados WHERE id={id}'
+        cursor.execute(sql)
+
+        nombreFoto = cursor.fetchone()[0]
+        borrarEstaFoto = os.path.join(app.config['UPLOADS'],nombreFoto)
+        print(borrarEstaFoto)
+
+        os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+    
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+    sql = f'UPDATE empleados SET nombre={_nombre}, correo={_correo} WHERE id={id}'
+    cursor.execute(sql)
+    conn.commit()
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
